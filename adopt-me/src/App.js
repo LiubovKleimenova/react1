ee149439c49c48d340bbc0b24e656ac73077b3eb
LiubovKleimenova <kleimenovaliubov@gmail.com>
2021-10-13 20:53:30 -0400

    add packages

---
+++ adopt-me/src/App.js
@@ -0,0 +1,30 @@
+/* global React */
+
+const Pet = (props) => {
+	return React.createElement("div", {}, [
+		React.createElement("h2", {}, props.name),
+		React.createElement("h3", {}, props.animal),
+		React.createElement("h3", {}, props.breed),
+	]);
+};
+
+const App = () => {
+	return React.createElement(
+		"div",
+		{},
+		React.createElement("h1", { id: "my-brand" }, "Adopt Me"),
+		React.createElement(Pet, {
+			name: "Luna",
+			animal: "dog",
+			breed: "Havaneese",
+		}),
+		React.createElement(Pet, {
+			name: "Pepper",
+			animal: "bird",
+			breed: "Cockatiel",
+		}),
+		React.createElement(Pet, { name: "Sudo", animal: "dog", breed: "terrier" })
+	);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
