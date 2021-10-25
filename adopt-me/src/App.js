926a68bfa06a7f9a6e20568b02d892196b79eac4
LiubovKleimenova <kleimenovaliubov@gmail.com>
2021-10-25 19:18:46 -0400

    custom hook

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,30 +1,15 @@
-/* global React */
-
-const Pet = (props) => {
-	return React.createElement("div", {}, [
-		React.createElement("h2", {}, props.name),
-		React.createElement("h3", {}, props.animal),
-		React.createElement("h3", {}, props.breed),
-	]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+//import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-	return React.createElement(
-		"div",
-		{},
-		React.createElement("h1", { id: "my-brand" }, "Adopt Me"),
-		React.createElement(Pet, {
-			name: "Luna",
-			animal: "dog",
-			breed: "Havaneese",
-		}),
-		React.createElement(Pet, {
-			name: "Pepper",
-			animal: "bird",
-			breed: "Cockatiel",
-		}),
-		React.createElement(Pet, { name: "Sudo", animal: "dog", breed: "terrier" })
+	return (
+		<div>
+			<h1>Adopt Me</h1>
+			<SearchParams />
+		</div>
 	);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
